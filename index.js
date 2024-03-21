263711957a1ff86587abdf08d32a53a8a2b8ea2c
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-03-21 22:31:18 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+      const num1 = 31;
+      const num2 = 2;
+      const multiply = num1 * num2;
+      multiply;
+
+     const random = Math.floor(Math.random() * 100) + 1;
+
+      const num3 = 24;
+      const num4 = 5;
+      const mod = num3 % num4;
+      
+      const numA = 7;
+      const numB = 2;
+      const numC = 20;
+      const numD = 4;
+      const max = Math.max(numA, numB, numC, numD);
+
+ 
